36880757c7eded88efffa252e136123e49dbf557
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2016-07-10 00:24:37 -0600

    Added service to keep the application pool awake

---
+++ HonduRutas/HonduRutas.ServiceInterface/Services/AwakeService.cs
@@ -0,0 +1,13 @@
+﻿using HonduRutas.ServiceModel.Requests;
+using ServiceStack;
+
+namespace HonduRutas.ServiceInterface.Services
+{
+    public class AwakeService : Service
+    {
+        public string Get(AwakenRequest request)
+        {
+            return "API is awake! Cheers!";
+        }
+    }
+}
---
+++ HonduRutas/HonduRutas.ServiceModel/Requests/AwakeRequests.cs
@@ -0,0 +1,15 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonduRutas.ServiceModel.Requests
+{
+    [Route("/awaken", "GET")]
+    public class AwakenRequest
+    {
+
+    }
+}

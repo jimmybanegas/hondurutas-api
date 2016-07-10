193286c626ab3b8b0c2b0850efafc779c1418c67
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2016-07-09 22:33:17 -0600

    Added dependency injection for the Data layer in the WebServices project

---
+++ HonduRutas/HonduRutas/Adapters/AutofacContainerAdapter.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using ServiceStack.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonduRutas.Adapters
+{
+    /// <summary>
+    /// Autofac implementation of the ServiceStack's ICOntainerAdapter
+    /// </summary>
+    public class AutofacContainerAdapter : IContainerAdapter
+    {
+        private readonly IContainer container;
+
+        public AutofacContainerAdapter(IContainer container)
+        {
+            this.container = container;
+        }
+
+        public T Resolve<T>()
+        {
+            return container.Resolve<T>();
+        }
+
+        public T TryResolve<T>()
+        {
+            T result;
+
+            if (container.TryResolve<T>(out result))
+            {
+                return result;
+            }
+
+            return default(T);
+        }
+    }
+}
--- HonduRutas/HonduRutas/AppHost.cs
+++ HonduRutas/HonduRutas/AppHost.cs
@@ -1,11 +1,21 @@
 ﻿using Funq;
 using ServiceStack;
 using HonduRutas.ServiceInterface;
+using Autofac;
+using ServiceStack.Text;
+using System.Collections.Generic;
+using HonduRutas.Adapters;
+using HonduRutas.Data;
+using HonduRutas.Data.ContractImplementation;
+using HonduRutas.Domain.Contracts;
+using System.Data.Entity;
 
 namespace HonduRutas
 {
     public class AppHost : AppHostBase
     {
+        private readonly ICollection<string> allowedOrigins = new[] { "*" };
+
         /// <summary>
         /// Default constructor.
         /// Base constructor requires a name and assembly to locate web service classes. 
@@ -18,14 +28,28 @@ namespace HonduRutas
 
         /// <summary>
         /// Application specific configuration
-        /// This method should initialize any IoC resources utilized by your web service classes.
         /// </summary>
-        /// <param name="container"></param>
         public override void Configure(Container container)
         {
-            //Config examples
-            //this.Plugins.Add(new PostmanFeature());
-            //this.Plugins.Add(new CorsFeature());
+            var builder = new ContainerBuilder();
+            JsConfig.EmitCamelCaseNames = true;
+            Plugins.Add(new CorsFeature(
+                allowedHeaders: "Content-Type",
+                allowCredentials: true,
+                allowOriginWhitelist: allowedOrigins
+            ));
+
+            ConfigureDataDependencies(builder);
+
+            container.Adapter = new AutofacContainerAdapter(builder.Build());
+        }
+
+        private void ConfigureDataDependencies(ContainerBuilder builder)
+        {
+            var context = new APIContext();
+            builder.RegisterInstance(context).As<DbContext>();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>));
         }
     }
 }

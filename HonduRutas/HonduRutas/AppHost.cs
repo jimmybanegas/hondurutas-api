0f406ecd66b2d51e0bd09919c2793885557e604b
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2016-07-10 10:57:35 -0600

    Removed Hello sample service

--- HonduRutas/HonduRutas/AppHost.cs
+++ HonduRutas/HonduRutas/AppHost.cs
@@ -1,6 +1,5 @@
 ﻿using Funq;
 using ServiceStack;
-using HonduRutas.ServiceInterface;
 using Autofac;
 using ServiceStack.Text;
 using System.Collections.Generic;
@@ -9,8 +8,8 @@ using HonduRutas.Data;
 using HonduRutas.Data.ContractImplementation;
 using HonduRutas.Domain.Contracts;
 using System.Data.Entity;
-using System;
 using HonduRutas.Application.Services.Routes;
+using HonduRutas.ServiceInterface.Services;
 
 namespace HonduRutas
 {
@@ -23,7 +22,7 @@ namespace HonduRutas
         /// Base constructor requires a name and assembly to locate web service classes. 
         /// </summary>
         public AppHost()
-            : base("HonduRutas", typeof(MyServices).Assembly)
+            : base("HonduRutas", typeof(RouteService).Assembly)
         {
 
         }

7e19161a3a77044658c664a8ab41d410b3650481
Jimmy Ramos <jimmy.ramos@unitec.edu>
2016-07-09 21:22:32 -0600

    stop times

--- HonduRutas.Domain/Entities/StopTime.cs
+++ HonduRutas.Domain/Entities/StopTime.cs
@@ -5,6 +5,27 @@ namespace HonduRutas.Domain.Entities
     //This entity is required
     public class StopTime : BaseEntity
     {
-        
+        public int TripId { get; set; }
+
+        public string ArrivalTime { get; set; }
+
+        public string DepartureTime { get; set; }
+
+        public string StopId { get; set; }
+
+        public int StopSequence { get; set; }
+
+        public string StopHeadsign { get; set; }
+
+        public int? PickupType { get; set; }
+
+        public int? DropOffType { get; set; }
+
+        public string ShapeDistTraveled { get; set; }
+
+        public virtual Stop Stop { get; set; }
+
+        public virtual Trip Trip { get; set; }
+
     }
 }
